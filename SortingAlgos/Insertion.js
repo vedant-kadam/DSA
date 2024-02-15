16b8168c7137f877e117a47f00e6897c5309a8ee
vedant-Dscission <106381831+vedant-Dscission@users.noreply.github.com>
2024-02-16 00:09:28 +0530

    insertion sort

---
+++ SortingAlgos/Insertion.js
@@ -0,0 +1,19 @@
+let arr = [25, 12, 0, 58, 3, 69, 4];
+
+function insertion() {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > curr) {
+      console.log("in while start", arr);
+      arr[j + 1] = arr[j];
+      console.log("in while end", arr);
+      j = j - 1;
+    }
+    arr[j + 1] = curr;
+    console.log("Out while", arr);
+  }
+  console.log(arr);
+}
+
+insertion();
